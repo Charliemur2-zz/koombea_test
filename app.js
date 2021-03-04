1045aed1e89616119f9225ee937a2185faa7f3f5
Charliemur2 <charliemurciamunoz@gmail.com>
2021-03-04 10:27:45 -0500

    add: x-close buttonto navbar and delete unused svg

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const navBar = document.querySelector('.js-navbar');
 const header = document.querySelector('.js-header');
 const menuBtn = document.querySelector('.js-hamburguer-btn');
+const closeBtn = document.querySelector('.js-x-btn');
 
 /* scrolled menu */
 window.onscroll = function() {
   if(window.pageYOffset > 80) {
     header.classList.add('header--scrolled');
     menuBtn.classList.add('hamburger-btn--scrolled');
+    closeBtn.classList.add('x-btn--scrolled');
 
     if(!scrolled) {
       navbar.style.transform = 'translateY(-70px)';
@@ -19,6 +21,7 @@ window.onscroll = function() {
   } else {
     header.classList.remove('header--scrolled');
     menuBtn.classList.remove('hamburger-btn--scrolled');
+    closeBtn.classList.remove('x-btn--scrolled');
     scrolled = false;
   }
 };
@@ -26,11 +29,16 @@ window.onscroll = function() {
 /* click on hamburger menu */
 function showMenu() {
   if (navBar.classList.contains('navbar', 'js-navbar')) {
+    menuBtn.classList.add('hamburger-btn--clicked');
+    closeBtn.classList.add('x-btn');
+    closeBtn.classList.remove('x-btn--clicked');
     navBar.classList.remove('navbar');
     navBar.classList.add('navbar--mobile');
     disableScroll();
   } else {
-    
+    menuBtn.classList.remove('hamburger-btn--clicked');
+    closeBtn.classList.remove('x-btn');
+    closeBtn.classList.add('x-btn--clicked');
     navBar.classList.remove('navbar--mobile');
     navBar.classList.add('navbar');
     enableScroll();
